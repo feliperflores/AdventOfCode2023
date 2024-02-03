63b6d5b4f32f4a8230565e89651acfd2bbb3e174
Felipe Flores <feliperflores2@hotmail.com>
2024-02-03 20:37:52 -0300

    Fix scripts and move rearrange folders

--- day1/firstPart/main.js
+++ day1/firstPart/main.js
@@ -1,7 +1,8 @@
 import { readFile } from "fs";
 import getCalibration from "./sumOfAllCalibrations.js";
+import path from "path";
 
-readFile('./input.txt', (err, data) => {
+readFile(path.resolve("./day1/input.txt"), (err, data) => {
   const values = data.toString().split("\r\n");
   console.log(getCalibration(values));
 });
